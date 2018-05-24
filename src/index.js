43309dbbcf4ab37042e677816431575b5ed41068
AriellaRogueN <maitilydevidasi@gmail.com>
2018-05-24 16:40:11 -0400

    llevar datos al dom desde index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 encodeGetData = () => {
     let offset = parseInt(document.getElementById("offset").value);
     let string = document.getElementById("string").value;
-    cipher.encode(offset, string);
+    document.getElementById("messageDiv").innerHTML = cipher.encode(string, offset); 
+
 }
 
 decodeGetData = () => {
     let offset = parseInt(document.getElementById("offset").value);
     let string = document.getElementById("string").value;
-    cipher.decode(offset, string);
+    document.getElementById("messageDiv").innerHTML = cipher.decode(string, offset); 
 }
