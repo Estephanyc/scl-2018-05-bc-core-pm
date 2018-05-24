44bfd05333a7e99e417b040cd23d3a5f6c3db017
AriellaRogueN <maitilydevidasi@gmail.com>
2018-05-24 10:11:59 -0400

    Lista la funcion descrifrar

--- src/cipher.js
+++ src/cipher.js
@@ -6,22 +6,22 @@ window.cipher = {
     for (let i = 0; i < string.length; i ++) 
     {
       
-      //convertir a mayusculas el string
+      // 1.convertir a mayusculas el string
       string = string.toUpperCase();
 
-      // 1.convertir a codigo ASCII el caracter
+      // 2.convertir a codigo ASCII el caracter
       let toASCCII = string.charCodeAt(i);  
       
-      // 2.Obtener la nueva posicion en ASCCII
+      // 3.Obtener la nueva posicion en ASCCII
       var newPosition = ((toASCCII - 65 + offset) % 26 ) + 65;
       
-      // No remplazar el caracter de los espacios
+      // 4.No remplazar el caracter de los espacios
       if (toASCCII == 32){ newPosition = 32; }
       
-      //3.convertir de ASCCII a caracter la nueva posicion
+      // 5.convertir de ASCCII a caracter la nueva posicion
       let fromASCCII = String.fromCharCode(newPosition);
 
-      //mostrando los nuevos caracteres
+      // 6.Mostrando los nuevos caracteres
       document.getElementById("message").innerHTML =
       document.getElementById("message").innerHTML + fromASCCII;
     }
@@ -30,7 +30,27 @@ window.cipher = {
   ,
   decode: (offset, string) => 
   {
-    
+    for (let i = 0; i < string.length; i++) {
+
+      // 1.convertir a mayusculas el string
+      string = string.toUpperCase();
+
+      // 2.convertir a codigo ASCII el caracter
+      let toASCCII = string.charCodeAt(i);
+
+      // 3.Obtener la nueva posicion en ASCCII
+      var newPosition = ((toASCCII + 65 - offset) % 26) + 65;
+
+      // 4.No remplazar el caracter de los espacios
+      if (toASCCII == 32) { newPosition = 32; }
+
+      // 5..convertir de ASCCII a caracter la nueva posicion
+      let fromASCCII = String.fromCharCode(newPosition);
+
+      // 6.Mostrando los nuevos caracteres
+      document.getElementById("message").innerHTML =
+      document.getElementById("message").innerHTML + fromASCCII;
+    }
   }
   
 }
