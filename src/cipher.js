c4d95ebb5850ce245156b4dc7c9822bd250d31db
AriellaRogueN <maitilydevidasi@gmail.com>
2018-05-24 16:24:37 -0400

    return de la funcion encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,56 +1,59 @@
 window.cipher = {
-  encode : (offset,string) =>
+  
+  encode : (string,offset) =>
    {
-    
-    // ciclo para recorrer todas los caracteres de la frase 
+    let message = [];
+      // ciclo para recorrer todas los caracteres de la frase 
     for (let i = 0; i < string.length; i ++) 
-    {
-      
+    {      
       // 1.convertir a mayusculas el string
       string = string.toUpperCase();
 
       // 2.convertir a codigo ASCII el caracter
-      let toASCCII = string.charCodeAt(i);  
+      const toASCCII = string.charCodeAt(i);  
       
       // 3.Obtener la nueva posicion en ASCCII
-      var newPosition = ((toASCCII - 65 + offset) % 26 ) + 65;
+      let newPosition = ((toASCCII - 65 + offset) % 26 ) + 65;
       
       // 4.No remplazar el caracter de los espacios
       if (toASCCII == 32){ newPosition = 32; }
       
       // 5.convertir de ASCCII a caracter la nueva posicion
-      let fromASCCII = String.fromCharCode(newPosition);
-
-      // 6.Mostrando los nuevos caracteres
-      document.getElementById("message").innerHTML =
-      document.getElementById("message").innerHTML + fromASCCII;
+      const fromASCCII = String.fromCharCode(newPosition);
+ 
+      // 6. agregando los caracteres a un vector
+      message[i] = fromASCCII; 
     }
-    
-  }
-  ,
-  decode: (offset, string) => 
+  // 7.Retornando el mensaje     
+  return message.join(""); 
+  },
+  
+  decode: (string,offset) => 
   {
-    for (let i = 0; i < string.length; i++) {
-
+    let message = [];
+    for (let i = 0; i < string.length; i++) 
+    {
       // 1.convertir a mayusculas el string
       string = string.toUpperCase();
 
       // 2.convertir a codigo ASCII el caracter
-      let toASCCII = string.charCodeAt(i);
+      const toASCCII = string.charCodeAt(i);
 
       // 3.Obtener la nueva posicion en ASCCII
-      var newPosition = ((toASCCII + 65 - offset) % 26) + 65;
+      let newPosition = ((toASCCII + 65 - offset) % 26) + 65;
 
       // 4.No remplazar el caracter de los espacios
       if (toASCCII == 32) { newPosition = 32; }
 
       // 5..convertir de ASCCII a caracter la nueva posicion
-      let fromASCCII = String.fromCharCode(newPosition);
+      const fromASCCII = String.fromCharCode(newPosition);
 
-      // 6.Mostrando los nuevos caracteres
-      document.getElementById("message").innerHTML =
-      document.getElementById("message").innerHTML + fromASCCII;
+      // 6. agregando los caracteres a un vector
+      message[i] = fromASCCII;
     }
+  // 7.Mostrando el nuevo mensaje
+  return message.join("");
+    
   }
   
 }
