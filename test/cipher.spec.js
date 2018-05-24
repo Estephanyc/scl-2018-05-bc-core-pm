ea00910dcbea053729d57d69a868b48e62f87a3e
AriellaRogueN <maitilydevidasi@gmail.com>
2018-05-24 19:56:47 -0400

    puse dos test adicionales

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -14,6 +14,10 @@ describe('cipher', () => {
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
       assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33),'HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
+
+    it('debería retornar "QF KNJXYF XJWF JQ OZJAJX" para "La fiesta sera el jueves" con offset 5', () => {
+      assert.equal(cipher.encode('La fiesta sera el jueves', 5), 'QF KNJXYF XJWF JQ OZJAJX');
+    });
   });
 
   describe('cipher.decode', () => {
@@ -25,6 +29,11 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
       assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG', 33),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
+
+    it('debería retornar "DESPUES DE LAS 8" para "IJXUZJX IJ QFX THMT" con offset 5', () => {
+      assert.equal(cipher.decode('IJXUZJX IJ QFX THMT', 5), 'DESPUES DE LAS OCHO');
+    });
+
   });
 
 });
