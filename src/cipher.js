90a8ae89a89a629ef60936dfaf151ecf6c0d1254
AriellaRogueN <maitilydevidasi@gmail.com>
2018-05-23 21:42:33 -0400

    Funcion codificar lista, estilos css y estructura html

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,36 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode : (offset,string) =>
+   {
+    
+    // ciclo para recorrer todas los caracteres de la frase 
+    for (let i = 0; i < string.length; i ++) 
+    {
+      
+      //convertir a mayusculas el string
+      string = string.toUpperCase();
+
+      // 1.convertir a codigo ASCII el caracter
+      let toASCCII = string.charCodeAt(i);  
+      
+      // 2.Obtener la nueva posicion en ASCCII
+      var newPosition = ((toASCCII - 65 + offset) % 26 ) + 65;
+      
+      // No remplazar el caracter de los espacios
+      if (toASCCII == 32){ newPosition = 32; }
+      
+      //3.convertir de ASCCII a caracter la nueva posicion
+      let fromASCCII = String.fromCharCode(newPosition);
+
+      //mostrando los nuevos caracteres
+      document.getElementById("message").innerHTML =
+      document.getElementById("message").innerHTML + fromASCCII;
+    }
+    
   }
+  ,
+  decode: (offset, string) => 
+  {
+    
+  }
+  
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-/* Acá va tu código */
+//Obtener data y llamar funciones respectivas
+encodeGetData = () => {
+    let offset = parseInt(document.getElementById("offset").value);
+    let string = document.getElementById("string").value;
+    cipher.encode(offset, string);
+}
+
+decodeGetData = () => {
+    let offset = parseInt(document.getElementById("offset").value);
+    let string = document.getElementById("string").value;
+    cipher.decode(offset, string);
+}
